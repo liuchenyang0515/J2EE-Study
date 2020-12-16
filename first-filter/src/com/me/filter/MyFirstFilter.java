e13926f28bd9a0018f6f75c8bb427dbc6005fb9e
liuchenyang0515 <897418643@qq.com>
2020-12-16 10:20:19 +0800

    1-5 过滤器的生命周期

--- first-filter/src/com/me/filter/MyFirstFilter.java
+++ first-filter/src/com/me/filter/MyFirstFilter.java
@@ -4,11 +4,22 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 public class MyFirstFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		System.out.println("过滤器已被销毁");
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		System.out.println("过滤器初始化成功");
+	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
