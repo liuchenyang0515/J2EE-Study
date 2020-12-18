6f6e86e66f602b9262174dec3a0f244518f0a91c
liuchenyang0515 <897418643@qq.com>
2020-12-18 15:01:39 +0800

    2-3 list迭代列表

---
+++ freemarker/bin/com/me/freemarker/sample2.ftl
@@ -0,0 +1,22 @@
+<#list computers as c>
+序号：${c_index + 1} <#-- 迭代变量_index保存了循环的索引，从0开始 -->
+SN：${c.sn}
+型号：${c.model}
+<#switch c.state>
+<#case 1>
+状态：使用中
+<#break>
+<#case 2>
+状态：闲置
+<#break>
+<#case 3>
+状态：已作废
+<#break>
+</#switch>
+<#if c.user??>
+用户：${c.user}
+</#if>
+采购日期：${c.dop?string("yyyy-MM-dd")}
+采购价格：${c.price?string("0.00")}
+----------------------------------------
+</#list>
--- freemarker/src/com/me/freemarker/FreemarkerSample1.java
+++ freemarker/src/com/me/freemarker/FreemarkerSample1.java
@@ -2,8 +2,10 @@ package com.me.freemarker;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.me.freemarker.entity.Computer;
@@ -25,17 +27,25 @@ public class FreemarkerSample1 {
 		// 设置加载的目录,在这个类中加载，""表示当前包
 		config.setClassForTemplateLoading(FreemarkerSample1.class, "");
 		// 得到模版对象
-		Template t = config.getTemplate("sample1.ftl");
+		Template t = config.getTemplate("sample2.ftl");
 		// 2.创建数据
 		Map<String, Object> data = new HashMap<String, Object>();
-		data.put("site", "百度");
-		data.put("url", "http://www.baidu.com");
-		data.put("date", new Date());
-		data.put("number", 1290451230.124384598);
-		Map<String, String> info = new HashMap<String, String>();
-		info.put("cpu", "i5");
-		Computer c1 = new Computer("1234567", "ThinkPad", 2, null, new Date(), 12900f, info);
-		data.put("computer", c1);
+		List<Computer> computers = new ArrayList<>();
+		computers.add(new Computer("1234567", "ThinkPad X1", 2, null, new Date(), 12999f, new HashMap()));
+		computers.add(new Computer("1234568", "HP X1", 1, "张三", new Date(), 7500f, new HashMap()));
+		computers.add(new Computer("1234569", "DELL X1", 3, "李四", new Date(), 8500f, new HashMap()));
+		computers.add(new Computer("1234570", "ACER X1", 1, "王五", new Date(), 6300f, new HashMap()));
+		computers.add(new Computer("1234571", "MSI X1", 1, "赵六", new Date(), 9300f, new HashMap()));
+		data.put("computers", computers);
+		
+//		data.put("site", "百度");
+//		data.put("url", "http://www.baidu.com");
+//		data.put("date", new Date());
+//		data.put("number", 1290451230.124384598);
+//		Map<String, String> info = new HashMap<String, String>();
+//		info.put("cpu", "i5");
+//		Computer c1 = new Computer("1234567", "ThinkPad", 2, null, new Date(), 12900f, info);
+//		data.put("computer", c1);
 		// 3.产生输出,先把字节流转换为字符流
 		t.process(data, new OutputStreamWriter(System.out));
 	}
