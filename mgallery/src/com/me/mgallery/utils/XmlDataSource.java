8d95100dc132d76a1d51dbbccd98681d69df4582
liuchenyang0515 <897418643@qq.com>
2020-12-21 15:09:44 +0800

    2-4 开发XmlDataSource-实现XML数据提取

--- mgallery/src/com/me/mgallery/utils/XmlDataSource.java
+++ mgallery/src/com/me/mgallery/utils/XmlDataSource.java
@@ -1,13 +1,19 @@
 package com.me.mgallery.utils;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import com.me.mgallery.entity.Painting;
+
 public class XmlDataSource {
 	// 通过static静态关键字保证数据全局唯一
-	private static List data = new ArrayList();
+	private static List<Painting> data = new ArrayList<>();
 	private static String dataFile;
 	static {
 		// 得到painting.xml文件完整物理路径
@@ -18,12 +24,45 @@ public class XmlDataSource {
 		try {
 			dataFile = decoder.decode(dataFile,"UTF-8");
 			System.out.println(dataFile);
-		} catch (UnsupportedEncodingException e) {
+			// 利用DOM4j对XML进行解析
+			SAXReader reader = new SAXReader();
+			// 1.获取Document文档对象
+			Document document = reader.read(dataFile);
+			// 2.Xpath得到XML节点集合
+			List<Node> nodes = document.selectNodes("/root/painting");
+			
+			for (Node node : nodes) {
+				Element element = (Element)node;
+				String id = element.attributeValue("id");
+				String pname = element.elementText("pname"); // 子节点文本
+				Painting painting = new Painting();
+				painting.setId(Integer.parseInt(id));
+				painting.setPname(pname);
+				painting.setCategory(Integer.parseInt(element.elementText("category")));
+				painting.setPrice(Integer.parseInt(element.elementText("price")));
+				painting.setPreview(element.elementText("preview"));
+				painting.setDescription(element.elementText("description"));
+				data.add(painting);
+				System.out.println(id + ":" + pname);
+			}
+			
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * 	获取所有油画Painting对象
+	 * @return	Painting List
+	 */
+	public static List<Painting> getRawData() {
+		return data;
+	}
+	
 	public static void main(String[] args) {
-		new XmlDataSource();
+//		new XmlDataSource();
+		List<Painting> ps = XmlDataSource.getRawData();
+		System.out.println(ps);
 	}
 }
