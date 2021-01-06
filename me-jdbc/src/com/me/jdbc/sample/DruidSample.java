b46a4251cde7da7fb3fb1781662ba33662e2acde
liuchenyang0515 <897418643@qq.com>
2021-01-06 15:10:11 +0800

    8-3 Druid连接池的配置与使用（二）

--- me-jdbc/src/com/me/jdbc/sample/DruidSample.java
+++ me-jdbc/src/com/me/jdbc/sample/DruidSample.java
@@ -32,7 +32,13 @@ public class DruidSample {
         try {
             // 2.获取DataSource数据源对象
             DataSource dataSource = DruidDataSourceFactory.createDataSource(properties);
+            // 3.创建数据库连接
             connection = dataSource.getConnection();
+            // 可以在下面打断点，然后在navicat上“工具”->“服务器监控”->MYSQL，勾选对应数据库刷新查看连接数目，正好是properties中设置的数目10
+//            for(int i = 0; i < 21; ++i) {
+//                // 测试连接分配最大数目，因为maxActive=20，这里会卡死，控制台不打印结果，因为第21个连接拿不到，程序不往下走了
+//                connection = dataSource.getConnection();
+//            }
             preparedStatement = connection.prepareStatement("select * from employee limit 0, 10");
             resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
@@ -45,8 +51,11 @@ public class DruidSample {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
+            /**
+             * 不使用连接池：connection.close()关闭连接
+             * 使用连接池：connection.close()将连接回收至连接池
+             */
             DbUtils.closeConnection(resultSet, preparedStatement, connection);
         }
-        // 3.创建数据库连接
     }
 }
