9631fe0cdefa66ea6f5e900e22040944330239df
liuchenyang0515 <897418643@qq.com>
2021-01-13 20:18:41 +0800

    6-1 MyBatis数据插入操作

--- mybatis/src/test/java/com/me/mybatis/MyBatisTestor.java
+++ mybatis/src/test/java/com/me/mybatis/MyBatisTestor.java
@@ -145,4 +145,32 @@ public class MyBatisTestor {
             MyBatisUtils.closeSession(sqlSession);
         }
     }
+
+    @Test
+    public void testInsert() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            Goods goods = new Goods();
+            goods.setTitle("测试商品");
+            goods.setSubTitle("测试子标题");
+            goods.setOriginalCost(200f);
+            goods.setCurrentPrice(100f);
+            goods.setDiscount(0.5f);
+            goods.setIsFreeDelivery(1);
+            goods.setCategoryId(43);
+            // insert()方法返回值代表本次成功插入的记录总数
+            int insertNum = sqlSession.insert("goods.insert", goods);
+            sqlSession.commit(); // 提交事务数据
+            System.out.println(goods.getGoodsId());
+        } catch (Exception e) {
+            if (sqlSession != null) {
+                sqlSession.rollback(); // 回滚事务
+            }
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
 }
