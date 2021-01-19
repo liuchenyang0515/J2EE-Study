916da49e1ffeba208a19aa1da333a169673e562e
liuchenyang0515 <897418643@qq.com>
2021-01-19 20:06:12 +0800

    3-21 查看容器内对象--调试观看，了解多个匿名对象使用方法

--- s04/src/main/java/com/imooc/spring/ioc/SpringApplication.java
+++ s04/src/main/java/com/imooc/spring/ioc/SpringApplication.java
@@ -12,18 +12,18 @@ public class SpringApplication {
         System.out.println(company);
         String website = company.getInfo().getProperty("website");
         System.out.println(website);
-//        System.out.println("==============================");
-//        //获取容器内所有beanId数组
-//        String[] beanNames = context.getBeanDefinitionNames();
-//        for (String beanName:beanNames){
-//            System.out.println(beanName);
-//            System.out.println("类型:" + context.getBean(beanName).getClass().getName());
-//            System.out.println("内容:" + context.getBean(beanName));
-//        }
-//
-//        Computer computer = context.getBean("com.imooc.spring.ioc.entity.Computer", Computer.class);
-//        System.out.println(computer.getBrand());
-//        Computer computer1 = context.getBean("com.imooc.spring.ioc.entity.Computer#1", Computer.class);
-//        System.out.println(computer1.getBrand());
+        System.out.println("==============================");
+        //获取容器内所有beanId数组，建议调试观看
+        String[] beanNames = context.getBeanDefinitionNames();
+        for (String beanName:beanNames){
+            System.out.println(beanName);
+            System.out.println("类型:" + context.getBean(beanName).getClass().getName());
+            System.out.println("内容:" + context.getBean(beanName));
+        }
+        // 对于多个匿名对象的使用办法，不推荐匿名对象，顺序容易混乱，建议加上beanId
+        Computer computer = context.getBean("com.imooc.spring.ioc.entity.Computer", Computer.class); // 等同com.imooc.spring.ioc.entity.Computer#0
+        System.out.println(computer.getBrand());
+        Computer computer1 = context.getBean("com.imooc.spring.ioc.entity.Computer#1", Computer.class);
+        System.out.println(computer1.getBrand());
     }
 }
