9eb66c2f65cbeb330f01f44e58a91f21ba9f2f9c
liuchenyang0515 <897418643@qq.com>
2021-01-27 09:52:14 +0800

    5-5 Resource注解按名称装配--修改注释，方便理解

--- s07/src/main/java/com/imooc/spring/ioc/service/DepartmentService.java
+++ s07/src/main/java/com/imooc/spring/ioc/service/DepartmentService.java
@@ -12,13 +12,14 @@ public class DepartmentService {
      * 2. @Resource未设置name属性
      * 2.1 以属性名作为bean name在IoC容器中匹配bean,如有匹配则注入
      * 2.2 按属性名未匹配,则按类型进行匹配,同@Autowired,需加入@Primary解决类型冲突
-     * 使用建议:在使用@Resource对象时推荐设置name或保证属性名与bean名称一致
+     * 使用建议:在使用@Resource对象时推荐设置name或保证属性名与beanId名称一致
+     * 而beanId默认是对应bean对象类名首字母小写
      */
 //    @Resource(name = "userOracleDao")
 //    private IUserDao udao;
     // =====等同于下面
 //    @Resource
-//    private IUserDao userOracleDao;
+//    private IUserDao userOracleDao; // 其实相当于private IUserDao userOracleDao = new UserOracleDao();
 
     @Resource
     private IUserDao userDao;
