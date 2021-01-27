29e1d911275fd0a5cc7c61e333668de13c943fd5
liuchenyang0515 <897418643@qq.com>
2021-01-27 16:15:39 +0800

    3-3 Controller方法参数接收请求参数--get请求自定义参数对照关系@RequestParam("......")，post请求只要input的name值和接收方法参数名相同就可以自动注入

--- first-springmvc/src/main/java/com/me/springmvc/controller/URLMappingController.java
+++ first-springmvc/src/main/java/com/me/springmvc/controller/URLMappingController.java
@@ -1,10 +1,7 @@
 package com.me.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/um") // 全局通用请求映射，就是get和post方法全局的访问前缀，get请求变为访问localhost/um/g
@@ -18,13 +15,20 @@ public class URLMappingController {
 
     @GetMapping("/g")
     @ResponseBody
-    public String getMapping() {
+    /* http://localhost/um/g?manager_name=lily */
+    // @RequestParam("manager_name"),自定义参数对照关系，请求中manager_name，动态注入到requestName，就不要求名称对应了
+    public String getMapping(@RequestParam("manager_name") String requestName) {
+        System.out.println("requestName:" + requestName);
         return "this is get method";
     }
 
     @PostMapping("/p") // 这里用index.html表单演示
     @ResponseBody
-    public String postMapping() {
+    // 只要保证input的name值和这里的变量名对应，SpringMVC就自动注入，如果参数名不对应，则为null
+    public String postMapping(String username, String password) {// 如果密码是纯数字，这里参数类型可以设置Long password，SpringMVC会自动转换
+        // 之前servlet还需要request.getParameter来获取
+        // SpringMVC就是为了简化web应用的开发难度
+        System.out.println(username + ":" + password);
         return "this is post method";
     }
 }
