1df4199523ef1e9e0e4e0dca369c0b47c385a527
liuchenyang0515 <897418643@qq.com>
2021-01-29 14:48:52 +0800

    2-1 实现RESTful实验室

--- restful/src/main/java/com/me/restful/controller/RestfulController.java
+++ restful/src/main/java/com/me/restful/controller/RestfulController.java
@@ -1,9 +1,7 @@
 package com.me.restful.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/restful")
@@ -12,5 +10,24 @@ public class RestfulController {
     @ResponseBody
     public String doGetRequest() {
         return "{\"message\":\"返回查询结果\"}";
+    }
+
+    @PostMapping("/request") // uri相同，请求方式不同，一个post一个get，并不冲突
+    @ResponseBody
+    public String doPostRequest() {
+        return "{\"message\":\"数据新建成功\"}";
+    }
+
+    @PutMapping("/request")
+    @ResponseBody
+    public String doPutRequest() {
+        return "{\"message\":\"数据更新成功\"}";
+    }
+
+
+    @DeleteMapping("/request")
+    @ResponseBody
+    public String doDeleteRequest() {
+        return "{\"message\":\"数据删除成功\"}";
     }
 }
