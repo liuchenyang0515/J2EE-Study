1eba2d7079b19375ba135c166efbac0c44c84f36
liuchenyang0515 <897418643@qq.com>
2021-01-30 20:58:12 +0800

    1-2 Interceptor使用技巧--多个拦截器的执行顺序流程

--- interceptor/src/main/java/com/imooc/restful/interceptor/MyInterceptor1.java
+++ interceptor/src/main/java/com/imooc/restful/interceptor/MyInterceptor1.java
@@ -6,18 +6,17 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MyInterceptor implements HandlerInterceptor {
+public class MyInterceptor1 implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println(request.getRequestURL() + "-准备执行");
-        response.getWriter().print("123123"); // 可以在这里设置响应体内容
-        return false;// true是请求前置处理完毕后拦截器放行，请求处理接着向后流转执行，false立即结束，产生响应返回客户端
+        System.out.println(request.getRequestURL()+"-准备执行-1");
+        return true;
     }
 
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println(request.getRequestURL() + "-目标处理成功");
+        System.out.println(request.getRequestURL()+"-目标处理成功-1");
     }
 
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println(request.getRequestURL() + "-响应内容已产生");
+        System.out.println(request.getRequestURL()+"-响应内容已产生-1");
     }
 }
